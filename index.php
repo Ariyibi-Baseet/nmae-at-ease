32632b9634879a56831bdea435115f6618869ba7
Bastocurrency1999 <ariyibi.baseet@gmail.com>
2021-04-05 14:28:15 -0700

    faq is now working

--- index.php
+++ index.php
@@ -296,7 +296,7 @@ include("./forms/userLogin.php");
 
     <!-- ======= F.A.Q Section ======= -->
     <!---------------------------------------------------- FAQ is not working ----------------------------------------->
-    <section id="faq" class="faq" style="display:none;">
+    <section id="faq" class="faq">
       <div class="container">
 
         <div class="section-title">
@@ -466,9 +466,9 @@ include("./forms/userLogin.php");
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
